37f9ddd39982243e6952f7881319426af6a9c4a6
parkerduckworth <parkerduckworth@gmail.com>
2017-11-29 23:13:55 -0800

    python binary search

--- Python/binary_search.py
+++ Python/binary_search.py
@@ -10,11 +10,3 @@ def binary_search(arr, val):
 		return binary_search(arr[:len(arr) // 2], val)
 	if val > mid:
 		return binary_search(arr[len(arr) // 2 + 1:], val)
-
-
-arr = [1, 4, 6, 44, 243, 6754, 2, 65]
-print(binary_search(arr, 1000)) # False
-print(binary_search(arr, 1)) # True
-print(binary_search(arr, -1)) # False
-print(binary_search(arr, 44)) # True
-print(binary_search(arr, 0)) # False
