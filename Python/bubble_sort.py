ae27c70813f03561dba0f75dc701be45eb41ce8a
parkerduckworth <parkerduckworth@gmail.com>
2017-11-29 23:08:28 -0800

    python insertion sort

--- Python/bubble_sort.py
+++ Python/bubble_sort.py
@@ -4,3 +4,7 @@ def bubble_sort(list_):
             if list_[j] > list_[j + 1]:
                 list_[j], list_[j + 1] = list_[j + 1], list_[j]
     return list_
+
+
+a = [5, 3, 7, 5, 8, 1, 5, 9, 2, 0]
+print(bubble_sort(a))
