e017805a83194fbda478b5cbc658dc62fd825ae8
parkerduckworth <parkerduckworth@gmail.com>
2017-11-29 23:09:19 -0800

    python bubble sort

--- Python/bubble_sort.py
+++ Python/bubble_sort.py
@@ -4,7 +4,3 @@ def bubble_sort(list_):
             if list_[j] > list_[j + 1]:
                 list_[j], list_[j + 1] = list_[j + 1], list_[j]
     return list_
-
-
-a = [5, 3, 7, 5, 8, 1, 5, 9, 2, 0]
-print(bubble_sort(a))
