d517ce282470bdbca496215f5230b09585a32708
parkerduckworth <parkerduckworth@gmail.com>
2017-12-05 16:40:13 -0800

    add python quick sort

--- Python/quick_sort.py
+++ Python/quick_sort.py
@@ -1,14 +1,15 @@
 
-def quickSort(list_):
-   quickSortHelper(list_,0,len(list_)-1)
+def quick_sort(list_):
+   quick_sort_helper(list_,0,len(list_)-1)
+   return list_
 
-def quickSortHelper(list_,first,last):
-   if first<last:
+def quick_sort_helper(list_,first,last):
+   if first < last:
 
        splitpoint = partition(list_,first,last)
 
-       quickSortHelper(list_,first,splitpoint-1)
-       quickSortHelper(list_,splitpoint+1,last)
+       quick_sort_helper(list_,first,splitpoint-1)
+       quick_sort_helper(list_,splitpoint+1,last)
 
 
 def partition(list_,first,last):
@@ -36,6 +37,5 @@ def partition(list_,first,last):
    temp = list_[first]
    list_[first] = list_[right_pointer]
    list_[right_pointer] = temp
-
 
    return right_pointer
