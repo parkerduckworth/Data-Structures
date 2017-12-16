153097cb715b9258eda75c5c143188d4cbd93a5f
parkerduckworth <parkerduckworth@gmail.com>
2017-12-15 17:41:47 -0800

    rework LL

--- Python/linked_list.py
+++ Python/linked_list.py
@@ -16,7 +16,7 @@ class Node(object):
         self.data = d
 
 
-
+'''
 class LinkedList(object):
     def __init__(self, r = None):
         self.root = r
@@ -54,3 +54,46 @@ class LinkedList(object):
             else:
                 this_node = this_node.get_next()
         return None
+
+class Node:
+
+    def __init__(self, data=None, next_node=None):
+        self.data = data
+        self.next_node = next_node
+
+    def get_data(self):
+        return self.data
+
+    def set_data(self, new_data):
+        self.data = new_data
+
+    def get_next_node(self):
+        return self.next_node
+
+    def set_next_node(self, new_pointer):
+        self.net_node = new_pointer
+
+class LinkedList:
+
+    def __init__(self, head=None):
+        self.head = head
+        self.size = 0
+
+    def get_size(self):
+        return self.size
+
+    def add_node(self, data):
+        new_node = Node(data, self.head)
+        self.head = new_node
+        self.size += 1
+        return True
+
+
+
+    def print_nodes(self):
+        curr = self.head
+        while curr:
+            print(curr.data)
+            curr = curr.get_next_node()
+
+'''
