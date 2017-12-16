490d043ad7e08f466a018ea81cf4c75c99dc7d8f
parkerduckworth <parkerduckworth@gmail.com>
2017-12-16 08:37:28 -0800

    modify linked list python

--- Python/linked_list.py
+++ Python/linked_list.py
@@ -1,80 +1,23 @@
-class Node(object):
-    def __init__(self, d, n = None):
-        self.data = d
-        self.next_node = n
+class Node:
+    def __init__(self, data, next_node=None):
+        self.data = data
+        self.next_node = next_node
 
     def get_next(self):
         return self.next_node
 
-    def set_next(self, n):
-        self.next_node = n
-
-    def get_data(self):
-        return self.data
-
-    def set_data(self, d):
-        self.data = d
-
-
-'''
-class LinkedList(object):
-    def __init__(self, r = None):
-        self.root = r
-        self.size = 0
-
-    def get_size(self):
-        return self.size
-
-    def add(self, d):
-        new_node = Node(d, self.root)
-        self.root = new_node
-        self.size += 1
-
-    def remove(self, d):
-        this_node = self.root
-        prev_node = None
-        while this_node:
-            if this_node.get_data() == d:
-                if prev_node:
-                    prev_node.set_next(this_node.get_next())
-                else:
-                    self.root = this_node
-                self.size -= 1
-                return True         # data removed
-            else:
-                prev_node = this_node
-                this_node = this_node.get_next()
-        return False          # data not found
-
-    def find(self, d):
-        this_node = self.root
-        while this_node:
-            if this_node.get_data() == d:
-                return d
-            else:
-                this_node = this_node.get_next()
-        return None
-
-class Node:
-
-    def __init__(self, data=None, next_node=None):
-        self.data = data
+    def set_next(self, next_node):
         self.next_node = next_node
 
     def get_data(self):
         return self.data
 
-    def set_data(self, new_data):
-        self.data = new_data
+    def set_data(self, data):
+        self.data = data
 
-    def get_next_node(self):
-        return self.next_node
 
-    def set_next_node(self, new_pointer):
-        self.net_node = new_pointer
 
 class LinkedList:
-
     def __init__(self, head=None):
         self.head = head
         self.size = 0
@@ -82,18 +25,33 @@ class LinkedList:
     def get_size(self):
         return self.size
 
-    def add_node(self, data):
+    def add(self, data):
         new_node = Node(data, self.head)
         self.head = new_node
         self.size += 1
         return True
 
+    def remove(self, data):
+        this_node = self.head
+        prev_node = None
+        while this_node:
+            if this_node.get_data() == data:
+                if prev_node:
+                    prev_node.set_next(this_node.get_next())
+                else:
+                    self.head = this_node
+                self.size -= 1
+                return True         # data removed
+            else:
+                prev_node = this_node
+                this_node = this_node.get_next()
+        return False          # data not found
 
-
-    def print_nodes(self):
-        curr = self.head
-        while curr:
-            print(curr.data)
-            curr = curr.get_next_node()
-
-'''
+    def find(self, data):
+        this_node = self.head
+        while this_node:
+            if this_node.get_data() == data:
+                return data
+            else:
+                this_node = this_node.get_next()
+        return None
