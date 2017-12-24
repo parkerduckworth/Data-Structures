010d7498205e766080e4264ef4e50645a87ecf50
parkerduckworth <parkerduckworth@gmail.com>
2017-12-23 19:29:36 -0800

    update sort methods

--- Python/quick_sort.py
+++ Python/quick_sort.py
@@ -1,64 +1,12 @@
 
-# ========
-# In Place
-# ========
-
-def qsort_in_place(array):
-    qsort_helper(array,0,len(array)-1)
-    return array
-
-
-def qsort_helper(array,first,last):
-    if first < last:
-
-        splitpoint = partition(array,first,last)
-
-        qsort_helper(array,first,splitpoint-1)
-        qsort_helper(array,splitpoint+1,last)
-
-
-def partition(array,first,last):
-    pivot_value = array[first]
-
-    left_pointer = first + 1
-    right_pointer = last
-
-    done = False
-    while not done:
-
-        while left_pointer <= right_pointer and array[left_pointer] <= pivot_value:
-            left_pointer += 1
-
-        while array[right_pointer] >= pivot_value and right_pointer >= left_pointer:
-            right_pointer -= 1
-
-        if right_pointer < left_pointer:
-            done = True
-        else:
-            temp = array[left_pointer]
-            array[left_pointer] = array[right_pointer]
-            array[right_pointer] = temp
-
-    temp = array[first]
-    array[first] = array[right_pointer]
-    array[right_pointer] = temp
-
-    return right_pointer
-
-
-
-# ============================================
-# More efficient due to randomization of pivot
-# ============================================
-
 from random import randint
 
-def qsort_random_pivot(array):
-    if len(array) <= 1: return array
+def quick_sort(L):
+    if len(L) <= 1: return L
     smaller, equal, larger = [], [], []
-    pivot = array[randint(0, len(array)-1)]
+    pivot = L[randint(0, len(L)-1)]
 
-    for x in array:
+    for x in L:
         if x < pivot:
             smaller.append(x)
         elif x == pivot:
@@ -66,4 +14,9 @@ def qsort_random_pivot(array):
         else:
             larger.append(x)
 
-    return qsort_random_pivot(smaller) + equal + qsort_random_pivot(larger)
+    return quick_sort(smaller) + equal + quick_sort(larger)
+
+
+L = [-1000, 0, 9999, 43, -22, 53, 76, 3, 98, 113, -435, 0, 5]
+
+print(quick_sort(L)) # [-1000, -435, -22, 0, 0, 3, 5, 43, 53, 76, 98, 113, 9999]
