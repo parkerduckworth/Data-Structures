a5d6b087f7f58f4606b0a2de580747249e437d2c
parkerduckworth <parkerduckworth@gmail.com>
2018-02-03 16:44:06 -0800

    file clean-up

--- Python/bubble_sort.py
+++
@@ -1,6 +0,0 @@
-def bubble_sort(L):
-    for i in range(0, len(L) - 1):
-        for j in range(0, len(L) - 1 - i):
-            if L[j] > L[j + 1]:
-                L[j], L[j + 1] = L[j + 1], L[j]
-    return L
--- Python/insertion_sort.py
+++
@@ -1,7 +0,0 @@
-def insertion_sort(list_):
-    for i in range(len(list_)):
-        j = i
-        while j > 0 and (list_[j] < list_[j - 1]):
-            list_[j], list_[j - 1] = list_[j - 1], list_[j]
-            j = j - 1
-    return list_
